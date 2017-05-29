045b20b8e9a1c9e019ac57f75e012f12b14ff316
Karthik Meda <karthikreddymeda@gmail.com>
2017-05-29 09:03:53 +0530

    initial

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-var Main = require('Main');
+var TodoApp = require('TodoApp');
 
 // Load foundation
 $(document).foundation();
@@ -10,6 +10,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
-  <p>React Boiler Plate</p>,
+  <TodoApp />,
   document.getElementById('app')
 );
