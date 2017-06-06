9e95d35605045401e6ebfcdd40eba6f6bb03e303
Karthik Meda <karthikreddymeda@gmail.com>
2017-06-06 22:08:55 +0530

    firebase setup

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -2,7 +2,7 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const TestUtils = require('react-addons-test-utils');
 const expect = require('expect');
-const $ = require('jQuery');
+//const $ = require('jQuery');
 
 
 const TodoList = require('TodoList');
@@ -28,7 +28,7 @@ it("should render one Todo component for each todo item", ()=>{
   var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
 
   expect(todosComponents.length).toBe(todos.length);
-  
+
 });
 
 });
